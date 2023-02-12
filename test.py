4b27471db24bcee79b018ba9b756d4887a0e57f7
Kenny Lau <klau24@calpoly.edu>
2023-02-12 15:00:48 -0800

    working on adding lobbyist, orgs, and hearings

--- test.py
+++ test.py
@@ -10,18 +10,37 @@ class Neo4jDDDB:
     def close(self):
         self.driver.close()
 
-    def addLegislators(self):
+    def addLobbyist(self):
         with self.driver.session() as session:
-            session.execute_write(self._create_and_return_Legislators)
+            session.execute_write(self._create_and_return_Lobbyist)
+    
+    def addOrganizations(self):
+        with self.driver.session() as session:
+            session.execute_write(self._create_and_return_Organizations)
+    
+    def addHearings(self):
+        with self.driver.session() as session:
+            session.execute_write(self._create_and_return_Hearings)
 
     @staticmethod
-    def _create_and_return_Legislators( tx):
-        '''
-        LOAD CSV WITH HEADERS FROM 'file:///orders.csv' AS row
-            MERGE (order:Order {orderID: row.OrderID})
-            CREATE SET order.shipName = row.ShipName;
-        '''
-        result = tx.run("LOAD CSV WITH HEADERS FROM 'file:////Users/Kenny/Desktop/dd-api/data/legislator.csv' AS row "
+    def _create_and_return_Lobbyist(tx):
+        result = tx.run("LOAD CSV WITH HEADERS FROM 'https://github.com/klau24/dd-api/blob/main/data/legislator.csv' AS row "
+                        "CREATE (person:Person:Legislator) "
+                        "SET person.pid = row.pid"
+                        )
+        return result
+    
+    @staticmethod
+    def _create_and_return_Organizations(tx):
+        result = tx.run("LOAD CSV WITH HEADERS FROM 'https://github.com/klau24/dd-api/blob/main/data/legislator.csv' AS row "
+                        "CREATE (person:Person:Legislator) "
+                        "SET person.pid = row.pid"
+                        )
+        return result
+
+    @staticmethod
+    def _create_and_return_Hearings(tx):
+        result = tx.run("LOAD CSV WITH HEADERS FROM 'https://github.com/klau24/dd-api/blob/main/data/legislator.csv' AS row "
                         "CREATE (person:Person:Legislator) "
                         "SET person.pid = row.pid"
                         )
@@ -30,5 +49,11 @@ class Neo4jDDDB:
 
 if __name__ == "__main__":
     neo4j_dddb = Neo4jDDDB(myCredentials.neo4j_dddb.hostname, myCredentials.neo4j_dddb.username, myCredentials.neo4j_dddb.password)
-    neo4j_dddb.addLegislators()
+    neo4j_dddb.addLobbyist()
+    neo4j_dddb.addOrganizations()
+    neo4j_dddb.addHearings()
+
+    neo4j_dddb.addPersonOrgEdge()
+    neo4j_dddb.addOrgHearingEdge()
+
     neo4j_dddb.close()
