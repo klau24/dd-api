fb497e2d39db10cce18971c2fd65c96bb8344ad8
Kenny Lau <klau24@calpoly.edu>
2023-02-21 13:54:06 -0800

    removed old file

--- populateKG.py
+++ populateKG.py
@@ -1,10 +1,11 @@
 from neo4j import GraphDatabase
-import dbConnect
+import time
 import myCredentials
 
 class Neo4jDDDB:
 
     def __init__(self, uri, user, password):
+        start_time = time.time()
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
         with self.driver.session() as session:
             # need to clean up csv to have correct quotations
@@ -49,10 +50,11 @@ class Neo4jDDDB:
             session.execute_write(self._create_LegislatorMotionEdge)
             print("Done")
         self.driver.close()
+        print("--- %s seconds ---" % (time.time() - start_time))
 
     @staticmethod
     def _create_Legislators(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://github.com/klau24/dd-api/raw/main/data/legislator.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/legislator.csv' AS row \
                 MERGE (person:Person:Legislator {pid: row.pid}) \
                     ON CREATE SET person.first = row.first, person.middle = row.middle, person.last = row.last, \
                     person.state = row.state, person.twitter_handle = row.twitter_handle, person.capitol_phone = row.capitol_phone, \
@@ -60,70 +62,70 @@ class Neo4jDDDB:
                 )
     @staticmethod
     def _create_Lobbyist(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/lobbyist.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/lobbyist.csv' AS row \
                 MERGE (person:Person:Lobbyist {pid: row.pid}) \
                     ON CREATE SET person.first = row.first, person.middle = row.middle, person.last = row.last, person.state = row.state;"
                 )
 
     @staticmethod
     def _create_Public(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/public.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/public.csv' AS row \
                 MERGE (person:Person:Public {pid: row.pid}) \
                     ON CREATE SET person.first = row.first, person.middle = row.middle, person.last = row.last;"
                 )
     
     @staticmethod
     def _create_Organizations(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/organizations.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/organizations.csv' AS row \
                 MERGE (org:Organization {oid: row.oid}) \
                     ON CREATE SET org.name = row.name, org.city = row.city;"
                 )
 
     @staticmethod
     def _create_Hearings(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/hearings.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/hearings.csv' AS row \
                 MERGE (hearing:Hearing {hid: row.hid}) \
                     ON CREATE SET hearing.date = row.date, hearing.state = row.state, hearing.type = row.type, hearing.session_year = row.session_year;"
                 )
 
     @staticmethod
     def _create_Utterances(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/utterances.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/utterances.csv' AS row \
                 MERGE (ut:Utterance {uid: row.uid}) \
                     ON CREATE SET ut.text = row.text, ut.time = row.time, ut.endTime = row.endTime, ut.type = row.type, ut.alignment = row.alignment, ut.state = row.state;"
                 )
     
     @staticmethod
     def _create_Committees(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/committee.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/committee.csv' AS row \
                 MERGE (c:Committee {cid: row.cid}) \
                     ON CREATE SET c.name = row.name, c.short_name = row.short_name, c.session_year = row.session_year, c.house = row.house, c.type = row.type, c.state = row.state;"
                 )
     
     @staticmethod
     def _create_Bills(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/bill.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/bill.csv' AS row \
                 MERGE (b:Bill {bid: row.bid}) \
                     ON CREATE SET b.type = row.type, b.number = row.number, b.bill_state = row.billState, b.status = row.status, b.house = row.house, b.state = row.state;"
                 )
 
     @staticmethod
     def _create_Motion(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/motion.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/motion.csv' AS row \
                 MERGE (m:Motion {mid: row.mid}) \
                     ON CREATE SET m.text = row.text, m.doPass = row.doPass, m.voteDate = row.VoteDate, m.ayes = row.ayes, m.naes = row.naes, m.abstain = row.abstain, m.result = row.result;"
                 )
 
     @staticmethod
     def _create_BillVersion(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/billVersion.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/billVersion.csv' AS row \
                 MERGE (bv:BillVersion {vid: row.vid}) \
                     ON CREATE SET bv.bid = row.bid, bv.date = row.date, bv.billState = row.billState, bv.subject = row.subject, bv.state = row.state;"   
                 )
 
     @staticmethod
     def _create_LobbyOrgEdge(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/lobbyist_works_for_org.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/lobbyist_works_for_org.csv' AS row \
                 MATCH (person:Person {pid: row.pid}) \
                 MATCH (org:Organization {oid: row.oid}) \
                 MERGE (person)-[r:WORKS_FOR]->(org);"   
@@ -131,7 +133,7 @@ class Neo4jDDDB:
 
     @staticmethod
     def _create_OrgHearingEdge(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/organization_participates_in_hearing.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/organization_participates_in_hearing.csv' AS row \
                 MATCH (org:Organization  {oid: row.oid}) \
                 MATCH (hearing:Hearing {hid: row.hid}) \
                 MERGE (org)-[r:PARTICIPATES_IN]->(hearing);"
@@ -139,7 +141,7 @@ class Neo4jDDDB:
     
     @staticmethod
     def _create_PersonUtteranceEdge(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/person_spoke_utterance.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/person_spoke_utterance.csv' AS row \
                 MATCH (person:Person  {pid: row.pid}) \
                 MATCH (ut:Utterance {uid: row.uid}) \
                 MERGE (person)-[r:SPOKE]->(ut);"
@@ -147,7 +149,7 @@ class Neo4jDDDB:
     
     @staticmethod
     def _create_LegislatorCommitteeEdge(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/legislator_memberOf_committee.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/legislator_memberOf_committee.csv' AS row \
                 MATCH (person:Person  {pid: row.pid}) \
                 MATCH (c:Committee {cid: row.cid, session_year: row.year}) \
                 MERGE (person)-[r:IS_MEMBER_OF]->(c);"
@@ -155,7 +157,7 @@ class Neo4jDDDB:
     
     @staticmethod
     def _create_CommitteeHearingEdge(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/committee_presentAt_hearing.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/committee_presentAt_hearing.csv' AS row \
                 MATCH (c:Committee  {cid: row.cid}) \
                 MATCH (h:Hearing {hid: row.hid}) \
                 MERGE (c)-[r:PRESENT_AT]->(h);"
@@ -163,7 +165,7 @@ class Neo4jDDDB:
 
     @staticmethod
     def _create_BillHearingEdge(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/bill_isDiscussedIn_hearing.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/bill_isDiscussedIn_hearing.csv' AS row \
                 MATCH (b:Bill  {bid: row.bid}) \
                 MATCH (h:Hearing {hid: row.hid}) \
                 MERGE (b)-[r:IS_DISCUSSED_IN]->(h);"
@@ -171,7 +173,7 @@ class Neo4jDDDB:
 
     @staticmethod
     def _create_MotionBillEdge(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/motion.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/motion.csv' AS row \
                 MATCH (m:Motion {mid: row.mid}) \
                 MATCH (b:Bill {bid: row.bid}) \
                 MERGE (m)-[r:RELATES_TO]->(b);"
@@ -179,7 +181,7 @@ class Neo4jDDDB:
     
     @staticmethod
     def _create_BillHasVersionEdge(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/billVersion.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/billVersion.csv' AS row \
                 MATCH (b:Bill {bid: row.bid}) \
                 MATCH (bv:BillVersion {vid: row.vid}) \
                 MERGE (b)-[r:HAS]->(bv);"
@@ -187,7 +189,7 @@ class Neo4jDDDB:
     
     @staticmethod
     def _create_LegislatorMotionEdge(tx):
-        tx.run("LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/klau24/dd-api/main/data/billVersion.csv' AS row \
+        tx.run("LOAD CSV WITH HEADERS FROM 'https://media.githubusercontent.com/media/klau24/dd-api/main/data/billVersion.csv' AS row \
                 MATCH (p:Person {pid: row.pid}) \
                 MATCH (m:Motion {mid: row.mid}) \
                 MERGE (p)-[r:VOTES]->(m);"
